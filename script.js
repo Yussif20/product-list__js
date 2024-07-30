429871aa2947d709350963990e478e0145047406
Yousef ayman <yousefayman2000500@gmail.com>
2024-07-30 16:37:45 +0300

    adding the item dynamically pt.2

--- script.js
+++ script.js
@@ -1,36 +1,10 @@
-const dessertContainer =document.querySelector(".dessert__container-items");
-
-fetch("./data.json").then(res=>{
-    if(!res.ok){
-        throw new Error("Fetching data failed" +res.statusText)
-    } return res.json()
-}).then(data=>{ 
-   console.log(data)
-    data.forEach(item=>{
-        dessertContainer.insertAdjacentHTML("afterbegin",
-            createItem(item["image"]["desktop"],item["image"]["tablet"],item["image"]["mobile"],item["category"],item["name"],item["price"])
-        )
-    })
-})
-
 const createItem =(imgDesktop,imgTablet,imgMobile,type,name,price) =>{ 
-    return (
-        `
-        <div class="item">
+  const item=
+        `<div class="item">
             <picture>
-             <source
-                src="${imgDesktop}"
-                media="(max-width:500px)"
-              />
-              <source
-                src="${imgTablet}"
-                media="(max-width:900px)"
-              />
-              <source
-                src="${imgMobile}"
-                media="(max-width:1500px)"
-              />
-              <img src="${imgDesktop}" alt="name"/>
+              <source srcset="${imgMobile}" media="(max-width: 400px)" />
+              <source srcset="${imgTablet}" media="(max-width: 900px)" />
+              <img src="${imgDesktop}" alt="${name}" />
             </picture>
             <button class="add-to-cart">
              <img src="images/icon-add-to-cart.svg" alt="cart icon" />
@@ -38,9 +12,20 @@ const createItem =(imgDesktop,imgTablet,imgMobile,type,name,price) =>{
             </button>
             <h4>${type}</h4>
             <h2>${name}</h2>
-            <p>${price}</p>
-        </div>
-    `
-    )
-  
+            <p>$ ${price}</p>
+        </div>`
+    return item;
 }
+const dessertContainer =document.querySelector(".dessert__container-items");
+
+fetch("./data.json").then(res=>{
+    if(!res.ok){
+        throw new Error("Fetching data failed" +res.statusText)
+    } return res.json()
+}).then(data=>{ 
+   [...data].map(item=>{
+        dessertContainer.insertAdjacentHTML("beforeend",
+            createItem(item.image.desktop,item.image.tablet,item.image.mobile,item.category,item.name,item.price)
+        )
+    })
+})
